936c961dc73db121cf9cd6ffd2c71025da8a0546
Almir Jr <almirafjr.dev@gmail.com>
2021-10-08 15:11:04 -0300

    Create new 'toDos' using 'Todo' objects | Create methods to: refresh page, remove list item, close list item, open list item and delete all list items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  public tittle: String = 'My chores';
+  public toDos: Todo[] = [];
+
+  //Creates new Todo objects
+  constructor() {
+    this.toDos.push(new Todo(1, 'Learn basic programming', false));
+    this.toDos.push(new Todo(2, 'Learn how to git', false));
+    this.toDos.push(new Todo(3, 'Learn how to use javascript', false));
+    this.toDos.push(new Todo(4, 'Learn how to use typescript', false));
+    this.toDos.push(new Todo(5, 'Learn how to use angular', false));
+  }
+
+  //Refreshes current page
+  refreshPage() {
+    location.reload();
+  }
+
+  //Remove chore from the list
+  removeListItem(chore: Todo) {
+    const index = this.toDos.indexOf(chore);
+
+    //Security Conditional
+    if (index !== -1) {
+      this.toDos.splice(index, 1);
+    }
+  }
+
+  //Close chore if user mark as done
+  closeListItem(chore: Todo) {
+    chore.isDone = true;
+  }
+  //Open chore if user mark as undone (undone is the default value)
+  openListItem(chore: Todo) {
+    chore.isDone = false;
+  }
+
+  //Remove all chores from the list
+  deleteAllListItems() {
+    this.toDos = [];
+  }
 }
