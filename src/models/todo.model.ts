390abb1e5ca473c8035f1d2a1f438a65d0436b9b
Almir Jr <almirafjr.dev@gmail.com>
2021-10-08 15:06:24 -0300

    Add Todo class and constructor

---
+++ src/models/todo.model.ts
@@ -0,0 +1,8 @@
+export class Todo {
+    //Todo class constructor, eases understanding and repairing the code
+    constructor (
+        public id: Number,
+        public chore: String,
+        public isDone: Boolean
+    ) {}
+}
